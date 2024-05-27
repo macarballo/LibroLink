31624c38c33aba50868be780cfd4072f889afa0b
kgpelayo <kgpelayo1@up.edu.ph>
2024-05-28 06:40:50 +0800

    YAWA TESTINGA KO GOD
    touchign search, search result component, and app trying for search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,20 @@ import {
 } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
 import Header from './components/header/Header.jsx';
-<<<<<<< Updated upstream
-=======
 import Book_info from './pages/book_info/Book_info.jsx';
 import Search_result_component from './components/search_resut_component/Search_result_component.jsx';
 import Search from './components/search/Search.jsx';
->>>>>>> Stashed changes
 
 function App() {
+  
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home/>} />
-<<<<<<< Updated upstream
-=======
         <Route path="/book-info" element={<Book_info/>} />
         <Route path="/search-result" element={<Search_result_component />} />
         <Route path="/search" element={<Search />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
