9d68283a92787e759c4d711836d92415acd86fb7
Meekah Yzabelle A. Carballo <macarballo@up.edu.ph>
2024-05-28 04:29:48 +0800

    display fetched new release books
    not responsive. next will be the integration of pages that allows user to view information about each book.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
 import Header from './components/header/Header.jsx';
+import Book_info from './pages/book_info/Book_info.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/book-info" element={<Book_info/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/book_info/Book_info.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/header/Header';
+import './book_info.css';
+
+function Book_info() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default  Book_info;
+
